597aed0831d938d40bba4ec1114d46f113dd53d5
@SAYO <91386506+sayo-api@users.noreply.github.com>
2025-06-09 01:14:03 -0300

    Create MainActivity.java

---
+++ src/main/java/com/example/anyapi/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.anyapi;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://anyapi.com.br");
+        setContentView(webView);
+    }
+}
